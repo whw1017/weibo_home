7f8c1deb0da5df3143cce93dca480bc7c9621392
whw <348644381@qq.com>
2019-04-02 21:23:54 +0800

    完成用户注册功能

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -22,7 +22,15 @@ class UsersController extends Controller
             'email'=>'required|email|unique:users|max:255',
             'password'=>'required|confirmed|min:6'//confirmed 保证两次输入的密码一致
         ]);
-        return ;
+
+        $user = User::create([
+            'name'=>$request->name,
+            'email'=>$request->email,
+            'password'=>bcrypt($request->password)
+        ]);
+
+        session()->flash('success','欢迎进入');
+        return redirect()->route('users.show',[$user]);
     }
 
 }
